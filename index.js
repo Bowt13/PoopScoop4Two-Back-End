8e16d1d9206c8dc5365ae940dc68576353c8feb5
nsch1 <nick.schooneman1@gmail.com>
2018-03-06 12:56:49 +0100

    Add basic user router with basic /users get path

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const userRouter = require('./users/router')
 
 const port = process.env.PORT || 4001
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(userRouter)
 
 app.listen(port, () => console.log(`Express listening on port: ${port}`))
