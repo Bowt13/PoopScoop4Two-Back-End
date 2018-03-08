3fa83cb2a3f4da070eec80ddad0fc453979db32d
nsch1 <nick.schooneman1@gmail.com>
2018-03-08 12:55:54 +0100

    Committing to pull

--- seeders/20180306113004-demo-user-basic.js
+++ seeders/20180306113004-demo-user-basic.js
@@ -1,6 +1,97 @@
 'use strict';
+const Fakerator = require('fakerator')
+const fakerator = Fakerator()
 
 const bcrypt = require('bcrypt')
+
+
+const breeds = [
+  "husky",
+  "keeshond",
+  "lhasa",
+  "malamute",
+  "redbone",
+  "shiba",
+  "whippet",
+  "vizsla",
+  "samoyed",
+  "saluki",
+  "rottweiler",
+  "coonhound",
+  "chow",
+  "chihuahua",
+  "clumber",
+  "akita",
+  "bouvier",
+  "boxer",
+  "cairn",
+  "dachshund",
+  "dingo",
+  "doberman",
+  "appenzeller",
+  "borzoi",
+  "germanshepherd",
+  "labrador",
+  "kuvasz",
+  "mexicanhairless",
+  "otterhound",
+  "papillon",
+  "pekinese",
+  "maltese",
+  "lhasa",
+  "malinois",
+  "leonberg"
+]
+
+const generateBreedStats = () => {
+  const breedStats = {}
+  for (var i = 0; i < 20; i++) {
+    const randomBreed = breeds[Math.floor(Math.random()*breeds.length)]
+    breedStats[randomBreed] = Math.floor(Math.random*70) - 20
+  }
+  return breedStats
+}
+
+const fakeUsers = []
+
+for (var i = 0; i < 150; i++) {
+  fakeUsers.push(
+    {
+      name: fakerator.names.name(),
+      email: fakerator.internet.email(),
+      password: bcrypt.hashSync('secret', 10),
+      breedStats: JSON.stringify(generateBreedStats())
+    }
+  )
+}
+
+fakeUsers.unshift({
+  name: 'Jack Russel',
+  email: 'jack@example.com',
+  password: bcrypt.hashSync('secret', 10),
+  breedStats: JSON.stringify(generateBreedStats())
+},
+{
+  name: 'Peter Dogtown',
+  breedStats: JSON.stringify(generateBreedStats()),
+  email: 'peter@example.com',
+  password: bcrypt.hashSync('secret', 10)
+},
+{
+  name: 'Lola Pugari',
+  breedStats: JSON.stringify(generateBreedStats()),
+  email: 'lola@example.com',
+  password: bcrypt.hashSync('secret', 10)
+})
+
+const newFakeUsers = fakeUsers.map(user => {
+  const totalVotes = user.breedStats.reduce((sum, breed) => {
+    return sum + Math.abs(breed.votes)
+  }, 0)
+
+  user.totalVotes = totalVotes
+  return user
+})
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
